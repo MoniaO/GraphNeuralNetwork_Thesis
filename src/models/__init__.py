9dce80b1e6e787d3c229fb407803ab5009847e21
Monia <monika@MacBook-Pro-Monia.local>
2026-06-30 23:10:08 +0200

    Update models

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,12 +1,12 @@
 from models.gcn import GCN
-from models.sage import GraphSAGE
-from models.gat import GAT
+#from models.sage import GraphSAGE
+#from models.gat import GAT
 
 def build_model(cfg, num_nodes):
     models = {
-        "gcn":  GCN,
-        "sage": GraphSAGE,
-        "gat":  GAT,
+        "gcn":  GCN #,
+        #"sage": GraphSAGE,
+        #"gat":  GAT,
     }
     if cfg.model.name not in models:
         raise ValueError(f"Uknown model: {cfg.model.name}")
