3c6b320908534f54559b1b8b813f8600cd3c6d83
Monia <monika@MacBook-Pro-Monia.local>
2026-07-09 12:09:24 +0200

    Update gcn.py - multilayer architecture

--- src/models/gcn.py
+++ src/models/gcn.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn.functional as F
-from torch.nn import Linear
+from torch.nn import Linear, ModuleList
 from torch_geometric.nn import GCNConv, global_mean_pool
 
 
@@ -8,18 +8,21 @@ class GCN(torch.nn.Module):
     def __init__(self, cfg, in_channels):
         super().__init__()
         hidden_dim = cfg.model.hidden_dim
+        num_layers = cfg.model.num_layers
 
-        self.conv1 = GCNConv(in_channels, hidden_dim)
-        self.conv2 = GCNConv(hidden_dim, hidden_dim)
+        self.convs = ModuleList()
+
+        self.convs.append(GCNConv(in_channels, hidden_dim))
+        for _ in range(num_layers - 1):
+            self.convs.append(GCNConv(hidden_dim, hidden_dim))
+
         self.lin = Linear(hidden_dim, 1)
 
     def forward(self, x, edge_index, batch):
-        x = self.conv1(x, edge_index)
-        x = F.relu(x)
-        x = self.conv2(x, edge_index)
-        x = F.relu(x)
+        for conv in self.convs:
+            x = conv(x, edge_index)
+            x = F.relu(x)
 
         x = global_mean_pool(x, batch)
         x = self.lin(x).view(-1)
-
         return x
