30af9ff72b249d48352763314355ea9e3e831b7a
Monia <monika@MacBook-Pro-Monia.local>
2026-07-11 23:23:33 +0200

    Update src/models/__init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,6 +2,7 @@ from models.gcn import GCN
 from models.sage import GraphSAGE
 from models.gat import GATNet
 from models.gin import GINNet
+from models.gnn_lp import SimpleHeteroGNN
 
 
 def build_model(cfg, in_channels):
@@ -9,7 +10,8 @@ def build_model(cfg, in_channels):
         "gcn":  GCN ,
         "sage": GraphSAGE,
         "gat":  GATNet,
-        "gin":  GINNet
+        "gin":  GINNet, 
+        "gnn_lp":  SimpleHeteroGNN
     }
 
     if cfg.model.name == "gcn":
@@ -20,5 +22,7 @@ def build_model(cfg, in_channels):
         return GINNet(cfg, in_channels)
     elif cfg.model.name == "gat":
         return GATNet(cfg, in_channels)
+    elif cfg.model.name == "gnn_lp":
+        return SimpleHeteroGNN(cfg, in_channels)
     else:
         raise ValueError(f"Unknown model: {cfg.model.name}")
